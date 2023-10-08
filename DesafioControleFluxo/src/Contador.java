8ffc014e0cf503e5a8413066ab9138f08951c697
Erick Sena <ericksena621@gmail.com>
2023-10-08 14:33:36 -0300

    feat: add desafio controle fluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int parametroUm = sc.nextInt();
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        sc.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
